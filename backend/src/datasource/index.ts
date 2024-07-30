b8022a2eb0688b28dc8edf3f780f65d73d774df4
lgrignon <louis.grignon@protonmail.com>
2024-07-30 16:25:20 +0200

    bonuses: forbid passwords in git + do not use superuser to connect to DB

--- backend/src/datasource/index.ts
+++ backend/src/datasource/index.ts
@@ -3,14 +3,16 @@ import { Tag } from "../entities/Tag";
 import { Category } from "../entities/Category";
 import { Ad } from "../entities/Ad";
 
+const dbPass = process.env.THEGOODCORNER_DBPASS;
+console.log("init TypeORM DS with password: " + dbPass)
 export const dataSource = new DataSource({
     type: 'postgres',
     
     host: 'db',
     port: 5432,
     database: 'the_good_corner',
-    username: 'postgres',
-    password: 'example',
+    username: 'the_good_corner_user',
+    password: dbPass,
 
     entities: ['src/entities/*.ts'],
     synchronize: true,
