4df8f4e70fa6e937df7653e3edcb5a0581115a9d
lgrignon <louis.grignon@conscience-robotics.com>
2024-11-26 19:39:21 +0100

    workflow e2e

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const url = 'http://frontend:3000'
+const url = 'http://front:3000'
 
 test('has title', async ({ page }) => {
   await page.goto(url);
