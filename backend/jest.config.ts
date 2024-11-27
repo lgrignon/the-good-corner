25634c6d2210c997e5689d1ad37d607ae8d88bd5
lgrignon <louis.grignon@conscience-robotics.com>
2024-11-27 09:32:03 +0100

    exclude out dir

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -5,7 +5,8 @@ const config: Config = {
     preset: 'ts-jest',
     setupFilesAfterEnv: [
         './src/__tests_mockTypeorm-config.ts'
-    ]
+    ],
+    testPathIgnorePatterns: ["/node_modules/", "/out/"]
 };
 
 export default config;
