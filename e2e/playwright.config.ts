c335d0f80fda7d49544c273e2bf2f2cc9789b66b
lgrignon <louis.grignon@conscience-robotics.com>
2024-11-27 09:47:42 +0100

    publish screeshots

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -13,6 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -29,7 +30,8 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'off',
+    // trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
